4722fa80ddbe59a762834e1573e106f5cbc444ef
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2017-01-22 22:48:44 +0530

    socket.io added and working, styling added but not complete

--- public/controllers/chatroom.js
+++ public/controllers/chatroom.js
@@ -5,4 +5,19 @@ angular.module('chatroomApp')
         $scope.user = user;
 
         $scope.user.a = $scope.user.name;
+
+
+
+        var socket = io();
+
+        $scope.sendMessage= function(){
+            console.log('i am logged');
+            console.log(socket);
+            socket.emit('chat message',$scope.user.message);
+        };
+
+        socket.on('chat message',function(msg){
+            console.log(msg);
+        });
+       
     });
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+// var express = require('express');
+
+// var app = express();
+
+
+
+
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
 var express = require('express');
-
+http = require('http');
 var app = express();
+var server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
 
 
-var server = require('http').Server(app);
+
 
 app.use(express.static('public'));
 
@@ -11,4 +24,17 @@ app.get('/',function(req,res){
     res.sendFile(__dirname + '/views' + '/index.html');
 });
 
-app.listen(3000);
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message',msg);
+  });
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000,function(){
+    console.log("Chatroom running on port 3000");
+});
